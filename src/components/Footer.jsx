b3c91f72dc13085f74a8ab1699b95ac3872460a7
njkir <njkir@LAPTOP-26QAFN9G>
2025-01-20 21:57:39 +1030

    Removed comment

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="bg-[#022574] text-white py-4 sm:py-6 md:py-8 text-center animate-fadeIn">
       <p className="text-sm sm:text-base md:text-lg">
-        &copy; 2024 NIra & Co. By Nirpadha Rayamajhi. All rights reserved. Made with ❤️
+        &copy; 2024 NIra & Co. By Nirpadha Rayamajhi. All rights reserved.
       </p>
     </footer>
   );
